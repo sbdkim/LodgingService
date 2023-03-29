23fda358c72f097c64b2609d72b0b70985860b3f
yeriJo <joyeri116@gamil.com>
2023-03-29 10:17:19 +0900

    QnaDAO 수정

--- src/main/java/com/ezen/biz/dao/QnaDAO.java
+++ src/main/java/com/ezen/biz/dao/QnaDAO.java
@@ -14,30 +14,30 @@ public class QnaDAO {
 	@Autowired
 	SqlSessionTemplate mybatis;
 	
-	@Override
+
 	public List<QnaVO> getListQna(String email){
 		return mybatis.selectList("QnaMapper.getListQna",email);
 	}
 	
-	@Override
+
 	public QnaVO getQna(int qseq) {
 		
 		return mybatis.selectOne("QnaMapper.getQna",qseq);
 	}
 	
-	@Override
+
 	public void insertQna(QnaVO vo) {
 		
 		mybatis.insert("QnaMapper.insertQna", vo);
 	}
 	
-	@Override
+
 	public List<QnaVO> getListAllQna() {
 		
 		return mybatis.selectList("QnaMapper.getListAllQna");
 	}
 	
-	@Override
+
 	public void updateQna(QnaVO vo) {
 		
 		mybatis.update("QnaMapper.updateQna", vo);
