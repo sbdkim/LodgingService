d69bc327747847e76fbed770df7e11a4a465442e
wonchan-ji <129017115+wonchan-ji@users.noreply.github.com>
2023-03-29 15:06:43 +0900

    header
    header 수정

--- src/main/java/com/ezen/view/MemberController.java
+++ src/main/java/com/ezen/view/MemberController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.ezen.biz.dto.MemberVO;
@@ -19,19 +17,19 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 
-	//login 페이지로 이동
+	// login 페이지로 이동
 	@GetMapping("/login_form")
 	public String loginView() {
 		return "member/login";
 	}
-	
+
 	@PostMapping("/login")
 	public String loginAction(MemberVO vo, Model model) {
 		int result = memberService.loginMember(vo);
-		
-		if(result == 1) {
+
+		if (result == 1) {
 			model.addAttribute("loginUser", memberService.getMember(vo.getEmail()));
-			
+
 			return "redirect:index";
 		} else {
 			return "member/login_fail";
@@ -76,29 +74,5 @@ public class MemberController {
 		memberService.insertMember(vo);
 		return "member/login";
 	}
-	
-	
-	/*
-	 * 사용자 인증
-	 */
-	@PostMapping("/login")
-	public String loginAction( MemberVO vo, 
-			 Model model) {
-		System.out.println(vo.getEmail());
-			System.out.println("loginAction():vo="+vo);
-		int result = memberService.loginMember(vo);
-		
-		if (result == 1) { // 로그인 성공
-			model.addAttribute("loginUser", memberService.getMember(vo.getEmail()));
-			
-			return "redirect:index";
-		} else {
-			return "member/login_fail";
-		}
-	}
-	
-	
-	
-	
 
 }
