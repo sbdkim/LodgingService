5087ae0f7b7010e10485ad8277d771649fb47d9a
Shin Bum (Daniel) Kim <dlkimxc@gmail.com>
2023-03-29 14:35:42 +0900

    login incomplet

--- src/main/java/com/ezen/view/MemberController.java
+++ src/main/java/com/ezen/view/MemberController.java
@@ -2,9 +2,13 @@ package com.ezen.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.ezen.biz.dto.MemberVO;
 import com.ezen.biz.service.MemberService;
 
 @Controller
@@ -14,9 +18,33 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	//login 페이지로 이동
 	@GetMapping("/login_form")
 	public String loginView() {
 		return "member/login";
 	}
+	
+	
+	/*
+	 * 사용자 인증
+	 */
+	@PostMapping("/login")
+	public String loginAction( MemberVO vo, 
+			 Model model) {
+		System.out.println(vo.getEmail());
+			System.out.println("loginAction():vo="+vo);
+		int result = memberService.loginMember(vo);
+		
+		if (result == 1) { // 로그인 성공
+			model.addAttribute("loginUser", memberService.getMember(vo.getEmail()));
+			
+			return "redirect:index";
+		} else {
+			return "member/login_fail";
+		}
+	}
+	
+	
+	
 	
 }
