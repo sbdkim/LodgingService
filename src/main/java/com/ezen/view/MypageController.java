503b40a9011065b966ceb48b926fd98c8d8a3c5f
wonchan-ji <129017115+wonchan-ji@users.noreply.github.com>
2023-03-31 10:36:37 +0900

    검색1차
    검색1차

--- src/main/java/com/ezen/view/MypageController.java
+++ src/main/java/com/ezen/view/MypageController.java
@@ -1,6 +1,5 @@
 package com.ezen.view;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -13,83 +12,74 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ezen.biz.dto.BookingVO;
 import com.ezen.biz.dto.MemberVO;
-import com.ezen.biz.dto.RoomVO;
 import com.ezen.biz.service.BookingService;
-
-import lombok.ToString;
 
 @Controller
 public class MypageController {
-	
+
 	@Autowired
 	private BookingService bookingService;
-	
+
 	@PostMapping("booking_insert")
 	public String insertBooking(BookingVO vo, HttpSession session) {
-		MemberVO loginUser = (MemberVO)session.getAttribute("loginUser");
-		
-		if(loginUser == null) {
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
 			return "member/login";
 		} else {
 			vo.setEmail(loginUser.getEmail());
 			bookingService.insertBooking(vo);
-			
+
 			return "redirect:booking_list";
 		}
 	}
-	
+
 	@GetMapping("/booking_list")
-	public String BookingListAction(HttpSession session, Model model,BookingVO vo) {
-		MemberVO loginUser = (MemberVO)session.getAttribute("loginUser");
-		
-		if(loginUser == null) {
+	public String BookingListAction(HttpSession session, Model model, BookingVO vo) {
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
 			return "member/login";
 		} else {
 			vo.setEmail(loginUser.getEmail());
 			vo.setStatus(0);
 			List<BookingVO> bookingList = bookingService.getListBookByEmail(vo);
-			
-			
+
 			model.addAttribute("bookingList", bookingList);
-			
+
 			return "mypage/bookingList";
 		}
 	}
-	
+
 	@GetMapping("/mypage")
 	public String myPageView(HttpSession session, BookingVO vo, Model model) {
-		
-		MemberVO loginUser = (MemberVO)session.getAttribute("loginUser");
-		
-		if(loginUser == null) {
+
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
 			return "member/login";
 		} else {
-						
-		
+
 			vo.setEmail(loginUser.getEmail());
 			vo.setBseq(vo.getBseq());
 			vo.setStatus(0);
 			vo.setRseq(vo.getRseq());
-			
+
 			List<BookingVO> bookList = bookingService.getListBookByEmail(vo);
-			
-					
+
 			model.addAttribute("bookingList", bookList);
-			
+
 			return "mypage/mypage";
-			
-			
-			
+
 		}
-		
-		
+
 	}
-	
+
 	@GetMapping("/booking_detail")
 	public String BookingDetail(HttpSession session, BookingVO vo, Model model) {
-		MemberVO loginUser = (MemberVO)session.getAttribute("loginUser");
-		
-		if(loginUser == null) {
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
 			return "member/login";
 		} else {
 //			vo.setEmail(loginUser.getEmail());
@@ -99,7 +89,7 @@ public class MypageController {
 //			
 //					
 //			model.addAttribute("bookingDetail", bookingDetail);
-			
+
 			return "mypage/bookingDetail";
 		}
 	}
