a2e68ea73c69b14e9b3bf6a579dcfbcbec275af4
yeriJo <joyeri116@gamil.com>
2023-04-04 10:20:31 +0900

    매출 수정중

--- src/main/java/com/ezen/biz/dto/SalesQuantity.java
+++ src/main/java/com/ezen/biz/dto/SalesQuantity.java
@@ -1,15 +1,25 @@
 package com.ezen.biz.dto;
 
-import lombok.Getter;
+
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
+
 public class SalesQuantity {
 	  
 	  private String aname;
-	  private int revenue;
+	  public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public int getRevenue() {
+		return revenue;
+	}
+	public void setRevenue(int revenue) {
+		this.revenue = revenue;
+	}
+	private int revenue;
   
 }
