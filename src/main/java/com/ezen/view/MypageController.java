ec89b2cbc795b92fcf49c69ab219ffafb32faf95
wonchan-ji <129017115+wonchan-ji@users.noreply.github.com>
2023-04-12 10:46:32 +0900

    결제하기
    결제하기

--- src/main/java/com/ezen/view/MypageController.java
+++ src/main/java/com/ezen/view/MypageController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ezen.biz.dto.AccommodationVO;
 import com.ezen.biz.dto.BookingVO;
@@ -26,7 +27,7 @@ public class MypageController {
 	@Autowired
 	private AccommodationService accommodationService;
 
-	@PostMapping("booking_insert")
+	@PostMapping("/booking_insert")
 	public String insertBooking(BookingVO vo, HttpSession session) {
 		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
 		if (loginUser == null) {
